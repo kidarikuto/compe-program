01bf6bcb424b585d3bb0e965e07e50255839877b
kidarikuto <Tan04tan20@gmail.com>
2024-05-05 13:05:24 +0900

    "test"

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 # import my_func
 import math 
 import sys
+import itertools
 from itertools import permutations
 from itertools import combinations
 from itertools import combinations_with_replacement
@@ -10,25 +11,23 @@ def val_cal(num):
     digit=len(c)
     return A*num+B*digit
 if __name__ == '__main__':
-    A,B,X=map(int,input().split())
-    ans=10**9
-    num_digit=1
-    # val_cal=A*ans+B+10
-    if val_cal(ans)<=X:
-        print(ans)
-    elif A+B>X:
-        print(0)
-    else:
-        next=int(ans/2)
-        while(1):
-            if val_cal(next)>X:
-                ans=next
-                next=int(next/2)
-                # print("next=",next)
-            elif val_cal(next)<=X:
-                next=int((next+ans)/2)
-                # print("next=",next)
-                # print("ans=",ans)
-                if ans-next==1 and val_cal(next)<=X:
-                    print(next)
-                    break
+    N=int(input())
+    L=list(map(int,input().split()))
+    L.sort()
+    # print(L)
+    ans=0
+    for a in range(N-2):
+        cnt=0
+        triangle=[]
+        for b in range(a+1,N-1):
+            for c in range(b+1,N):
+                l=[L[a],L[b],L[c]]
+                l.sort()
+                print(l)
+                if l[2] < l[0]+l[1]:
+                    cnt+=1
+                    triangle.append(l)
+        print(triangle)
+        ans=max(ans,cnt)
+
+    print(ans)
