69269faab2ee38c9e3d1ee98976640cbbfac63c4
kidarikuto <kida0718riku@icloud.com>
2024-05-13 08:50:10 +0900

    "readme修正"

--- test.py
+++ test.py
@@ -7,21 +7,25 @@ from itertools import permutations
 from itertools import combinations
 from itertools import combinations_with_replacement
 
-def val_cal(num):
-    c=str(num)
-    digit=len(c)
-    return A*num+B*digit
 if __name__ == '__main__':
-    N=int(input())
-    L=list(map(int,input().split()))
-    L.sort()
-    # print(L)
-    ans=0
-    cnt=0
-    triangle=[]
-    for a in range(N-2):
-        for b in range(a+1,N-1):
-            bisect
-    ans=max(ans,cnt)
+    N=input()
+    n=[int(c) for c in N]
+    # print(n)
+    digi=len(N)
+    ans=float("inf")
+    for i in range(1,1<<digi):
+        sum=0
+        cnt=0
+        for j in range(digi):
+            if i & (1<<j):
+                sum+=n[j]
+                cnt+=1
+        if sum%3==0:
+            del_num=digi-cnt
+            # print(del_num,sum,i)
+            ans=min(ans,del_num)
+    if ans==float("inf"):
+        print(-1)
+    else:
+        print(ans)
 
-    print(ans)
